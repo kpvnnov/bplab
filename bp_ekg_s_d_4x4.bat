4f31368a8903bce9f021239cc37e59225a322221
peter <peter@50aeb178-8500-dd11-b8ad-001a4d4a7278>
2002-04-18 14:30:43 +0000

    * от Андрея получины коэффициенты фильтра * КИХ фильтр 28 порядка, для канала ЭКГ. 15.04.2002. *        -209        -133         254         481          -9        -709 *        -376         960        1292        -701       -2722        -579 *        6319       12754       12754        6319        -579       -2722 *        -701        1292         960        -376        -709          -9 *         481         254        -133        -209 * получилось: *;КИХ фильтр 28 порядка, для канала ЭКГ. 15.04.2002. *CF_EKG:.word  -209, -133,  254, 481,   -9, -709 *	.word  -376,  960, 1292,-701,-2722, -579 *	.word  6319,12754,12754,6319, -579,-2722 *	.word  -701, 1292,  960,-376, -709,   -9 *	.word   481,  254, -133,-209 * начал пробовать прописывать 4x4 = 8Mb флеш, дописать процедуру записи/чтения

---
+++ bp_ekg_s_d_4x4.bat
@@ -0,0 +1,69 @@
+@echo off
+rem $Id: bp_ekg_s_d_4x4.bat,v 1.1 2002-04-18 14:30:42 peter Exp $
+echo Монитор давления с последовательной флэш 4 мбайта (1 микросхема)
+echo c экг, разводка платы сентябрь 2000 года (новая)
+echo с отладочными режимами
+echo версия для сертификации
+
+
+if exist flashprg\sclr4.obj goto sclr4
+echo =========================
+echo Making flashprg\sclr4.asm
+echo =========================
+asmtms\dspa.exe flashprg\sclr4.asm -l -v2xx -s -w
+:sclr4
+
+if exist flashprg\sera4.obj goto sera4
+echo =========================
+echo Making flashprg\sera4.asm
+echo =========================
+asmtms\dspa.exe flashprg\sera4.asm -l -v2xx -s -w
+:sera4
+
+if exist flashprg\sflw4.obj goto sflw4
+echo =========================
+echo Making flashprg\sflw4.asm
+echo =========================
+asmtms\dspa.exe flashprg\sflw4.asm -l -v2xx -s -w
+:sflw4
+
+if exist flashprg\spgm4.obj goto spgm4
+echo =========================
+echo Making flashprg\spgm4.asm
+echo =========================
+asmtms\dspa.exe flashprg\spgm4.asm -l -v2xx -s -w
+:spgm4
+
+if exist flashprg\sutils4.obj goto sutils4
+echo =========================
+echo Making flashprg\sutils4.asm
+echo =========================
+asmtms\dspa.exe flashprg\sutils4.asm -l -v2xx -s -w
+:sutils4
+
+echo =========================
+echo Making Bp_ekg_s_d.asm
+echo =========================
+asmtms\dspa.exe Bpekgsd.asm -l -v2xx -s -w -dVersionFerrum=2 -dNewFerrum=1 -dMem=44
+IF ERRORLEVEL 0 GOTO compile
+echo =========================
+echo Errors in assemble
+echo =========================
+goto end
+
+
+:compile
+if exist Bpekgsd.obj goto compile1
+echo =========================
+echo Bpekgsd.obj not found
+echo =========================
+goto end
+:compile1
+
+echo =========================
+echo Compile
+echo =========================
+asmtms\dsplnk bpekgsd.obj bp206.cmd -o bpekgsd.out -m bpekgsd.map
+del bpekgsd.obj
+
+:end
